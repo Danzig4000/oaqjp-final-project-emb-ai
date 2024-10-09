10941bdd5258d1694a9af06be8c0e61b1d11543c
Danzig4000 <91083663+Danzig4000@users.noreply.github.com>
2024-10-09 16:42:48 -0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+#URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+#Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+#Input json: { "raw_document": { "text": text_to_analyse } }
 def emotion_detector(text_to_analyze):
     #IBM Watson API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
